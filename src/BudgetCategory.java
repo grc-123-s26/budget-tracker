6c24ed37a7d6bf3ecacabbd02426dcacbc4e9a0c
JustinS720 <274596103+JustinS720@users.noreply.github.com>
2026-04-22 11:19:43 -0700

    Added constructor to BudgetCategory & getters

--- src/BudgetCategory.java
+++ src/BudgetCategory.java
@@ -1,3 +1,23 @@
 public class BudgetCategory {
-    
+    private String category;
+    private double limit;
+    private double spent;
+
+    public BudgetCategory(String category, double limit, double spent) {
+        this.category = category;
+        this.limit = limit;
+        this.spent = spent;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public double getLimit() {
+        return limit;
+    }
+
+    public double getSpent(){
+        return spent;
+    }
 }
