fd8e98668567f48f4ad642da27b96e6cb2dda43d
tk421 <42457203+tk421bsod@users.noreply.github.com>
2026-04-22 11:34:45 -0700

    Implement BudgetCategory constructor, getters

--- src/BudgetCategory.java
+++ src/BudgetCategory.java
@@ -1,3 +1,24 @@
 public class BudgetCategory {
+    private String name;
+    private double limit;
+    private double spent;
+
+    public BudgetCategory(String name, double limit, double spent) {
+        this.name = name;
+        this.limit = limit;
+        this.spent = spent;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getLimit() {
+        return this.limit;
+    }
+
+    public double getSpent() {
+        return this.spent;
+    }
     
 }
