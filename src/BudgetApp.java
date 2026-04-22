308410ff0ac14bde5be106672fa8ecd87a4bf351
uglybrecke <206612742+uglybrecke@users.noreply.github.com>
2026-04-22 12:05:59 -0700

    started rebuilding while loop but with the intent of making at array of budgets

--- src/BudgetApp.java
+++ src/BudgetApp.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -5,20 +6,36 @@ public class BudgetApp {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // while(scan.hasNextLine()) {
+        //     String category = scan.nextLine();
+        //     double limit = scan.nextDouble();
+        //     double spent = scan.nextDouble();
+
+        //     // Consume \n after spent input 
+        //     if(scan.hasNextLine()) scan.nextLine();
+
+        //     String limitString = String.format("$%.2f", limit);
+        //     String spentString = String.format("$%.2f", spent);
+        //     System.out.println("The budget limit for " + category + " was: " + limitString + 
+        //                        " but the actual spend was " + spentString);
+        // }
+
         while(scan.hasNextLine()) {
             String category = scan.nextLine();
-
             double limit = scan.nextDouble();
             double spent = scan.nextDouble();
+            
+            List<BudgetCategory> list = new ArrayList<>();
 
-            // Consume \n after spent input 
-            if(scan.hasNextLine()) scan.nextLine();
-
-            String limitString = String.format("$%.2f", limit);
-            String spentString = String.format("$%.2f", spent);
-            System.out.println("The budget limit for " + category + " was: " + limitString + 
-                               " but the actual spend was " + spentString);
+            list.add(new BudgetCategory(category, limit, spent));
         }
+
+
+        // BudgetCategory groceries = new BudgetCategory("Groceries", 500, 401);
+        // String testingTesting = groceries.toString();
+        // System.out.println(testingTesting);
+
+
     }
 
     /**
