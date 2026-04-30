9417f27f5225701c7466ad327f4106db27e610aa
HENRY <henry.garcia1120@icloud.com>
2026-04-30 15:24:14 -0700

    wave 1 implemented, going to begin testing

--- src/BudgetApp.java
+++ src/BudgetApp.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class BudgetApp {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in); 
 
         while(scan.hasNextLine()) {
             String category = scan.nextLine();
--- src/BudgetCategory.java
+++ src/BudgetCategory.java
@@ -1,3 +1,34 @@
 public class BudgetCategory {
     
+    // instance variables
+    private String name;
+    private double budgetLimit;
+    private double actualSpend;
+
+    // constructor
+    public BudgetCategory(String name, double budgetLimit, double actualSpend) {
+        this.name = name;
+        this.budgetLimit = budgetLimit;
+        this.actualSpend = actualSpend;
+    }
+
+    // getters
+    public String getName() {
+        return name;
+    }
+
+    public double getBudgetLimit() {
+        return budgetLimit;
+    }
+
+    public double getActualSpend() {
+        return actualSpend;
+    }
+
+    // toString method
+    public String toString() {
+        return "The budget limit for " + name + " was: $" + budgetLimit +
+               " but the actual spend was $" + actualSpend;
+    }
+
 }
